c1ffd34805de220d4f51d1d59d8f6504b7de2755
furk4nk <furkankip@hotmail.com>
2023-02-04 20:49:07 +0300

    Add project files.

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ClassMetotDemo
+{
+	public class Musteri
+	{
+		public int MusteriId { get; set; }
+		public string MusteriEMail { get; set; }
+		public string MusteriName { get; set; }
+		public int MusteriYaş { get; set; }
+	}
+	public class MusteriManager : Musteri, IManager<Musteri>
+	{
+		public void Delete(Musteri t)
+		{
+			Console.WriteLine("müşteri silindi ,{}",t.MusteriName);
+		}
+
+		public void Insert(Musteri t)
+		{
+			Console.WriteLine("müşteri eklendi",t.MusteriName);
+		}
+
+		public void Update(Musteri t)
+		{
+			Console.WriteLine("müşteri güncellendi "+t.MusteriName+t.MusteriEMail+t.MusteriYaş);
+		}
+
+		public List<Musteri> GetList()
+		{
+			Console.WriteLine("müşteri listesi");
+			List<Musteri> liste= new List<Musteri>();
+			Musteri m= new Musteri() { MusteriEMail = "abc", MusteriId = 1, MusteriName = "ahmet", MusteriYaş = 19 };
+			liste.Add(m);
+			return liste;
+		}
+	}
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿using ClassMetotDemo;
+
+MusteriManager m = new MusteriManager();
+
+foreach (var item in m.GetList())
+{
+	Console.WriteLine(item.MusteriName);
+	Console.WriteLine(item.MusteriEMail);
+	Console.WriteLine(item.MusteriYaş);
+}
